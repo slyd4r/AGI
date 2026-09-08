cf099ccde73d49fb195ba3a3a48e3537a8dde797
Mohamed Elmadani <Mohamed.Elmadani@agim.local>
2026-09-08 14:59:34 +0400

    2

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Paste the Apps Script deployment URL ending in /exec here. This URL is public.
-window.DASHBOARD_CONFIG = { apiUrl: "", refreshMinutes: 5 };
+window.DASHBOARD_CONFIG = { apiUrl: "https://script.google.com/macros/s/AKfycbxk70M_bRp8QexIC5NcrOW-UW1lNoX7SY9DloFtDgVmIFnUeoS5re0d6APCo0c2YrCj/exec", refreshMinutes: 5 };
